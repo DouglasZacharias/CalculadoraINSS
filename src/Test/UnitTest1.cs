ef23666d1c51f6360a7e2f7e1ed1f519082cfa55
Douglas Oliveira Zacharias <douglas.zacharias@cetil.com.br>
2021-01-19 13:58:15 -0300

    ajuste-nomenclatura-e-estrutura-de-pastas-e-arquivos

---
+++ src/Test/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculadora.CalculadoraINSS calc = new Calculadora.CalculadoraINSS();
+        [TestMethod]
+        public void Teste2011()
+        {
+            DateTime data = Convert.ToDateTime("25/12/2011");
+            Assert.AreEqual(calc.CalcularDesconto(data, 800M), 64M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 1200M), 108M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 1840M), 165.6M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 1900M), 209M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 3689M), 405.79M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 4000M), 405.86M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 5000M), 405.86M);
+        }
+        [TestMethod]
+        public void Teste2012()
+        {
+            DateTime data = Convert.ToDateTime("25/12/2012");
+            Assert.AreEqual(calc.CalcularDesconto(data, 800M), 56M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 1200M), 96M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 1840M), 165.6M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 1900M), 171M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 3689M), 405.79M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 4000M), 440M);
+            Assert.AreEqual(calc.CalcularDesconto(data, 5000M), 500M);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Exception),
+            "Não existe tabela cadastrada para a data solicitada.")]
+        public void TesteTabelaNaoCadastrada()
+        {
+            DateTime data = Convert.ToDateTime("25/12/2013");
+            calc.CalcularDesconto(data, 800M);
+        }
+    }
+}
